16169166fc7f8604efc82a2d5423bfc904399802
youthxp <lurenxp@qq.com>
2019-04-26 21:56:17 +0800

    执行时间统计

--- test.py
+++ test.py
@@ -11,6 +11,8 @@ import time
 
 
 if __name__ == '__main__':
-   con = ts.get_apis()
-   df = ts.get_stock_basics()
-   print(df)
+    t1 = time.perf_counter()
+    con = ts.get_apis()
+    df = ts.get_stock_basics()
+    t = time.perf_counter() - t1
+    print(df,t)
