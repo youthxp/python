3dd0d4e15bae155be928017964b954748b658787
youthxp <lurenxp@qq.com>
2019-04-26 22:32:33 +0800

    加入多线程库

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import tushare as ts
 import pandas as pd
 import time
+from multiprocessing.dummy import Pool
 
 
 
@@ -11,8 +12,11 @@ import time
 
 
 if __name__ == '__main__':
+    '''
+    测试
+    '''
     t1 = time.perf_counter()
     con = ts.get_apis()
     df = ts.get_stock_basics()
     t = time.perf_counter() - t1
-    print(df,t)
+    print(df)
